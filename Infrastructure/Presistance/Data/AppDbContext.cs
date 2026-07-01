abb9483c0ccabffb8d78ef0bd766b4f77c24ed29
mariam mahmoud <mariiiiiiiiiiio8@gmail.com>
2026-07-01 17:37:51 +0300

    Remove redundant DbSet properties from AppDbContext
    Removed DbSet properties for Categories, Products, Inventories, Orders, OrderDetails, and Transactions.

--- Infrastructure/Presistance/Data/AppDbContext.cs
+++ Infrastructure/Presistance/Data/AppDbContext.cs
@@ -12,13 +12,7 @@ namespace Infrastructure.Presistance.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-        public DbSet<Category> Categories => Set<Category>();
-        public DbSet<Product> products => Set<Product>();
-        public DbSet<Inventory> inventories => Set<Inventory>();
-        public DbSet<Order> orders => Set<Order>();
-        public DbSet<OrderDetail> OrderDetails => Set<OrderDetail>();
-        public DbSet<Transaction> transactions => Set<Transaction>();
-
+       
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
